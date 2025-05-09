a2376831a0a6aff721b41f3c8c4c5f3a2273655d
mahdi-vajdi <mahdivajdii@gmail.com>
2025-05-10 00:08:01 +0330

    feat(processing): add processor and worker

---
+++ internal/processing/processor.go
@@ -0,0 +1,142 @@
+package processing
+
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/mahdi-vajdi/go-image-processor/internal/model"
+	"github.com/mahdi-vajdi/go-image-processor/internal/repository"
+	"github.com/mahdi-vajdi/go-image-processor/internal/storage"
+)
+
+type ServiceConfig struct {
+	WorkerPoolSize  int
+	PollingInterval time.Duration
+	TaskBatchSize   int
+	TargetSizes     []int
+	TargetFormats   []string
+}
+
+type Service struct {
+	repo    repository.Repository
+	storage storage.Storage
+	config  ServiceConfig
+
+	taskChan chan model.ImageProcessingTask
+	wg       sync.WaitGroup
+	ctx      context.Context
+	cancel   context.CancelFunc
+}
+
+func NewService(repo repository.Repository, storage storage.Storage, config ServiceConfig) *Service {
+	if config.WorkerPoolSize <= 0 {
+		config.WorkerPoolSize = 5
+		log.Printf("Warning: WorkerPoolSize not set or invalid, defaulting to %d", config.WorkerPoolSize)
+	}
+	if config.PollingInterval <= 0 {
+		config.PollingInterval = 5 * time.Second
+		log.Printf("Warning: PollingInterval not set or invalid, defaulting to %s", config.PollingInterval)
+	}
+	if config.TaskBatchSize <= 0 {
+		config.TaskBatchSize = 10
+		log.Printf("Warning: TaskBatchSize not set or invalid, defaulting to %d", config.TaskBatchSize)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &Service{
+		repo:     repo,
+		storage:  storage,
+		config:   config,
+		taskChan: make(chan model.ImageProcessingTask, config.TaskBatchSize*2),
+		ctx:      ctx,
+		cancel:   cancel,
+	}
+}
+
+func (s *Service) Start() {
+	log.Println("Starting image processing service...")
+
+	s.wg.Add(1)
+	go s.dispatcher()
+
+	for i := 0; i < s.config.WorkerPoolSize; i++ {
+		s.wg.Add(1)
+		s.worker(i + 1) // Pass the ID
+	}
+
+	log.Printf("Image processing service started with %d workers and polling every %s", s.config.WorkerPoolSize, s.config.PollingInterval)
+}
+
+func (s *Service) Stop() {
+	log.Println("stopping image processing service...")
+
+	// Signal the dispatcher to stop
+	s.cancel()
+
+	done := make(chan struct{})
+	go func() {
+		s.wg.Wait() // Wait for all goroutines to finish
+		close(done) // Signal that all goroutines are finished
+	}()
+
+	// Wait for either all goroutines to finish or the provided shutdown context to expire
+	select {
+	case <-done:
+		log.Println("All image processing tasks finished")
+	case <-s.ctx.Done():
+		log.Println("Image processing service shutdown context timed out. Some tasks may not have finished.")
+	}
+
+	log.Println("Image processing service stopped.")
+}
+
+func (s *Service) dispatcher() {
+	defer s.wg.Done()
+
+	log.Println("Dispatcher started")
+
+	// Loop indefinitely until the service context is cancelled.
+	for {
+		select {
+		case <-s.ctx.Done():
+			log.Println("dispatcher received shutdown signal. Closing the task channel...")
+			close(s.taskChan)
+			return
+
+		default:
+			// Continue
+		}
+
+		tasks, err := s.repo.GetPendingTasks(s.ctx, s.config.TaskBatchSize)
+		if err != nil {
+			log.Printf("Error fetching pending task: %v. Retrying in %s", err, s.config.PollingInterval)
+			time.Sleep(s.config.PollingInterval)
+			continue // Start the next iteration
+		}
+
+		if len(tasks) == 0 {
+			log.Println("No pending tasks were found. Waiting...")
+			continue
+		}
+
+		log.Printf("Dispatcher fetched %d tasks", len(tasks))
+
+		// Send the fetched tasks to the task channel
+		for _, task := range tasks {
+			select {
+			case s.taskChan <- task:
+			// Task successfully was sent to the channel
+			case <-s.ctx.Done():
+				log.Printf("Dispatcher shutting down while sending tasks. Task %s might not be processed from this batch", task.ID)
+				close(s.taskChan)
+				return
+			}
+		}
+
+		// Wait for the polling interval before fetching the next batch
+		time.Sleep(s.config.PollingInterval)
+	}
+}
---
+++ internal/processing/worker.go
@@ -0,0 +1,126 @@
+package processing
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"image"
+	"log"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/disintegration/imaging"
+	"github.com/mahdi-vajdi/go-image-processor/internal/model"
+)
+
+func (s *Service) worker(id int) {
+	defer s.wg.Done()
+
+	log.Printf("Worker #%d started", id)
+
+	for task := range s.taskChan {
+		log.Printf("Worker #%d processing task %s (original: %s)...", id, task.ID, task.OriginalFilename)
+
+		processingCtx, cancel := context.WithTimeout(s.ctx, 10*time.Second)
+		err := s.repo.UpdateTaskStatus(processingCtx, task.ID, model.StatusProcessing, "")
+		cancel()
+
+		if err != nil {
+			log.Printf("Worker #%d failed to set the task %s to 'procssing': %v", id, task.ID, err)
+			continue
+		}
+
+		// Initialize the processing
+		processErr := s.processTask(s.ctx, &task)
+
+		// Update the task with the processing results
+		status := model.StatusCompleted
+		errorMessage := ""
+		if processErr != nil {
+			status = model.StatusFailed
+			errorMessage = processErr.Error()
+
+			log.Printf("Worker #%d: Task %s failed: %v", id, task.ID, processErr)
+		} else {
+			log.Printf("Worker #%d: Task %s completed successfully", id, task.ID)
+		}
+
+		updateContext, cancel := context.WithTimeout(s.ctx, 10*time.Second)
+		err = s.repo.UpdateTaskStatus(updateContext, task.ID, status, errorMessage)
+		cancel()
+
+		if err != nil {
+			// TODO: I need to create a cleanup process for this (maybe retry)
+			log.Printf("Worker #%d FATAL: failed to update task %s with final status '%s': %v", id, task.ID, status, err)
+		}
+	}
+
+	log.Printf("Worker @%d exiting.", id)
+}
+
+func (s *Service) processTask(ctx context.Context, task *model.ImageProcessingTask) error {
+	// Check if the context has been cancelled before starting or during long operations.
+	select {
+	case <-s.ctx.Done():
+		return s.ctx.Err()
+
+	default:
+		// Context is not done
+	}
+
+	log.Printf("Processing image for task %s (Storage key: %s)", task.ID, task.StorageKey)
+
+	// Download the file
+	originalImageReader, err := s.storage.Get(ctx, task.StorageKey)
+	if err != nil {
+		return fmt.Errorf("failed to donwload oringinal image %s: %w", task.StorageKey, err)
+	}
+	defer originalImageReader.Close()
+
+	// Decode the image
+	// Determine the image format based on the original file extension
+	img, format, err := image.Decode(originalImageReader)
+	if err != nil {
+		return fmt.Errorf("failed to decode image: %w", err)
+	}
+
+	log.Printf("Image decoded successfully (Format: %s, Size: %dx%d)", format, img.Bounds().Dx(), img.Bounds().Dy())
+
+	// Resize the image
+	targetWith := 800
+	resizedImage := imaging.Resize(img, targetWith, 0, imaging.Lanczos)
+
+	log.Printf("Image resized to %dx%d", resizedImage.Bounds().Dx(), resizedImage.Bounds().Dy())
+
+	// Encode the processed image
+	var buf bytes.Buffer
+	outputFormat := imaging.JPEG // TODO: this can be configurable by params
+	originalExt := filepath.Ext(task.OriginalFilename)
+
+	// New filename
+	// FIXME: use a better naming structure
+	processedFilename := fmt.Sprintf("%s_%dx%d.%s",
+		strings.TrimSuffix(task.OriginalFilename, originalExt),
+		resizedImage.Bounds().Dx(),
+		resizedImage.Bounds().Dy(),
+		strings.ToLower(outputFormat.String()),
+	)
+
+	err = imaging.Encode(&buf, resizedImage, outputFormat)
+	if err != nil {
+		return fmt.Errorf("failed to encode processed image: %w", err)
+	}
+
+	// Upload the processed image
+	processedStorageKey, err := s.storage.Save(ctx, processedFilename, &buf)
+	if err != nil {
+		return fmt.Errorf("failed to upload processed image %s: %w", processedFilename, err)
+	}
+
+	log.Printf("Processed image uploaded successfully with key: %s", processedStorageKey)
+
+	// TODO: save the details for the uploaded image in the database
+
+	return nil
+}
